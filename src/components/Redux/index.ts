01b7fe4ead1593ad3c0e7dbaf762547b77f1ff09
Pham Thi Kim Oanh <kimoanh2613@gmail.com>
2019-12-21 11:02:14 +0700

    fix conflict

--- src/components/Redux/index.ts
+++ src/components/Redux/index.ts
@@ -2,8 +2,6 @@ import {combineReducers,createStore,compose, applyMiddleware} from 'redux';
 import storeReducer from './reducers/store';
 import thunk from 'redux-thunk';
 
-<<<<<<< HEAD
-=======
 
 declare global {
   interface Window {
@@ -11,35 +9,23 @@ declare global {
   }
 }
 
->>>>>>> master
 const composeEnhancers =
   process.env.NODE_ENV !== "production" &&
   typeof window === "object" &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-<<<<<<< HEAD
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        shouldHotReload: false
-      })
-=======
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
->>>>>>> master
     : compose;
 
 const rootReducer=combineReducers({
     store : storeReducer
 })
 
-<<<<<<< HEAD
+
 export type State = ReturnType<typeof rootReducer>
 
 export const configStore =()=>{
-    const store =createStore(rootReducer,composeEnhancers([applyMiddleware(thunk)]));
-=======
-export type StateInit = ReturnType<typeof rootReducer>
+    const store =createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
 
-export const configStore =()=>{
-    const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
->>>>>>> master
     return store;
   }
 // export const configStore =()=>{
--- src/components/Redux/reducers/store.ts
+++ src/components/Redux/reducers/store.ts
@@ -3,12 +3,6 @@ import { configAction } from '../actions/configAction';
 import Store from '../../Types/store';
 
 interface StoreState {
-<<<<<<< HEAD
-    companys: Store | null
-}
-export const initStore: StoreState = {
-    companys: null,
-=======
     companys: Store | any[],
     isLoaded: boolean,
     error: any
@@ -17,7 +11,6 @@ export const initStore: StoreState = {
     companys: [],
     isLoaded: false,
     error: null
->>>>>>> master
 }
 
 const storeReducer = (state = initStore, action: configAction) => {
@@ -25,12 +18,8 @@ const storeReducer = (state = initStore, action: configAction) => {
         case types.GET_STORE:
             return {
                 ...state,
-<<<<<<< HEAD
-                companys: action.payload
-=======
                 companys: action.payload.datas,
                 isLoaded: true
->>>>>>> master
             };
         case types.LOAD_STORE_SUCCESS:
             return {
